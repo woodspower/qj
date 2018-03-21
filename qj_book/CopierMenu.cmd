da5b00c863acd1043e98cefc72504ff6c36bea3e
root <woodspower@gmail.com>
2018-03-22 00:45:03 +0800

    split check_and_init_book to multiple funcs

---
+++ qj_book/CopierMenu.cmd
@@ -0,0 +1,62 @@
+{
+    "InferenceName": "CopierMenu",
+    "InferenceFile": "/home/leo/qj/object_detection/data/CopierMenu/inference_ssd/frozen_inference_graph.pb",
+    "LabelMapFile": "/home/leo/qj/object_detection/data/CopierMenu/pascal_label_map.pbtxt",
+    "Sequence": [
+        {
+            "Name": "Select one submenu in Copier menu",
+            "KeyBody": [
+                {
+                    "Name": "Select Copier Submenu",
+                    "Conditions": {
+                            "Jobs": {"Bookname": "CopierSubmenu"},
+                            "Allow": {"Tags": "CopierOff"}
+                    },
+                    "Actions": [
+                        {
+                            "Command": "Click",
+                            "DecisionPeriod": 5000,
+                            "StartTag": "CopierOff"
+                        },
+                        {
+                            "Name": "Reload image",
+                            "Command": "Reload",
+                            "PresetPeriod": 2000
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "Name": "Go and do jobs in Copier Submenu",
+            "KeyBody": [
+                {
+                    "Name": "Goto Copier Submenu",
+                    "Conditions": [
+                        {
+                            "Name": "",
+                            "Allow": [
+                                {
+                                    "Name": "",
+                                    "Percent": 1.0,
+                                    "Tags": [
+                                        "CopierOn"
+                                    ]
+                                }
+                            ],
+                            "Disallow": []
+                        }
+                    ],
+                    "Actions": [
+                        {
+                            "Command": "Goto",
+                            "BookName": "CopierSubmenu"
+                        }
+                    ]
+                }
+            ]
+        }
+]
+}
+
+
---
+++ qj_book/Movie.cmd
@@ -0,0 +1,124 @@
+{
+    "InferenceName": "Movie",
+    "InferenceFile":"/home/leo/qj/object_detection/data/Movie/inference_ssd/frozen_inference_graph.pb",
+    "LabelMapFile":"/home/leo/qj/object_detection/data/Movie/pascal_label_map.pbtxt",
+    "Sequence": [
+        {
+            "Name": "One of Confirm->dNext->Cancel->Reject",
+            "KeyBody": [
+                {
+                    "Name": "Any kind of Confirm",
+                    "Conditions": [
+                        {
+                            "Name": "",
+                            "Allow": [
+                                {
+                                    "Name": "",
+                                    "Percent": 1.0,
+                                    "Tags": ["Confirm"]
+                                }
+                            ],
+                            "Disallow": []
+                        }
+                    ],
+                    "Actions": [
+                        {
+                            "Name": "Click center",
+                            "Command": "Click",
+                            "DecisionPeriod": 99,
+                            "StartTag": ["Confirm"],
+                            "StartOffset": [0.5, 0.5],
+                            "EndTag": [],
+                            "EndOffset": [],
+                            "Duration": "2~5"
+                        }
+                    ]
+                },
+                {
+                    "Name": "Dialog Next",
+                    "Conditions": [
+                        {
+                            "Name": "",
+                            "Allow": [
+                                {
+                                    "Name": "",
+                                    "Percent": 1.0,
+                                    "Tags": ["dNext"]
+                                }
+                            ],
+                            "Disallow": []
+                        }
+                    ],
+                    "Actions": [
+                        {
+                            "Name": "Click center",
+                            "Command": "Click",
+                            "DecisionPeriod": 99,
+                            "StartTag": ["dNext"],
+                            "StartOffset": [0.5, 0.5],
+                            "EndTag": [],
+                            "EndOffset": [],
+                            "Duration": "2~5"
+                        }
+                    ]
+                },
+                {
+                    "Name": "If Cancel",
+                    "Conditions": [
+                        {
+                            "Name": "",
+                            "Allow": [
+                                {
+                                    "Name": "",
+                                    "Percent": 1.0,
+                                    "Tags": ["Cancel"]
+                                }
+                            ],
+                            "Disallow": []
+                        }
+                    ],
+                    "Actions": [
+                        {
+                            "Name": "Click center",
+                            "Command": "Click",
+                            "DecisionPeriod": 99,
+                            "StartTag": ["Cancel"],
+                            "StartOffset": [0.5, 0.5],
+                            "EndTag": [],
+                            "EndOffset": [],
+                            "Duration": "2~5"
+                        }
+                    ]
+                },
+                {
+                    "Name": "Reject if nothing",
+                    "Conditions": [
+                        {
+                            "Name": "",
+                            "Allow": [
+                                {
+                                    "Name": "",
+                                    "Percent": 1.0,
+                                    "Tags": ["Reject"]
+                                }
+                            ],
+                            "Disallow": []
+                        }
+                    ],
+                    "Actions": [
+                        {
+                            "Name": "Click center",
+                            "Command": "Click",
+                            "DecisionPeriod": 99,
+                            "StartTag": ["Reject"],
+                            "StartOffset": [0.5, 0.5],
+                            "EndTag": [],
+                            "EndOffset": [],
+                            "Duration": "2~5"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
