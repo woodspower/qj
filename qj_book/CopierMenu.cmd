f2625e58b7727b2f45a1c196931c5656f8acb8db
root <woodspower@gmail.com>
2018-03-31 15:53:00 +0800

    add submenus

--- qj_book/CopierMenu.cmd
+++ qj_book/CopierMenu.cmd
@@ -4,22 +4,53 @@
         "Name": "Goto one of submenu from Copier/DailyCopier/MonsterTower",
         "KeyBody": [
             {
-                "Name": "Check Copier Submenu",
+                "Name": "Try CopierSubmenu",
                 "Conditions": {
                         "Jobs": {"Bookname": "CopierSubmenu"},
                         "Allow": {"Tags": ["CopierOff", "CopierOn"]}
                 },
                 "Actions": [
                     {   
-                        "Name": "Turn CopierOff to CopierOn",
-                        "Command": "Click",
-                        "StartTag": "CopierOff",
-                        "StopOnFail": "No" 
+                        "Name": "Select CopierSubmenu",
+                        "Command": "Click"
                     },
                     {
                         "Command": "Goto",
-                        "StartTag": "CopierOn",
                         "BookName": "CopierSubmenu" 
+                    }
+                ]
+            },
+            {
+                "Name": "Try DailyCopierSubmenu",
+                "Conditions": {
+                        "Jobs": {"Bookname": "DailyCopierSubmenu"},
+                        "Allow": {"Tags": ["CopierOff", "CopierOn"]}
+                },
+                "Actions": [
+                    {   
+                        "Name": "Select DailyCopierSubmenu",
+                        "Command": "Click"
+                    },
+                    {
+                        "Command": "Goto",
+                        "BookName": "DailyCopierSubmenu" 
+                    }
+                ]
+            },
+            {
+                "Name": "Try MonsterTowerSubmenu",
+                "Conditions": {
+                        "Jobs": {"Bookname": "MonsterTowerSubmenu"},
+                        "Allow": {"Tags": ["CopierOff", "CopierOn"]}
+                },
+                "Actions": [
+                    {   
+                        "Name": "Select MonsterTowerSubmenu",
+                        "Command": "Click"
+                    },
+                    {
+                        "Command": "Goto",
+                        "BookName": "MonsterTowerSubmenu" 
                     }
                 ]
             }
