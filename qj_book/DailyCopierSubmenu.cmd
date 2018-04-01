2e0361c6cb8556da6722b85784fabee46087b8b4
root <woodspower@gmail.com>
2018-04-01 13:48:14 +0800

    add new book

---
+++ qj_book/DailyCopierSubmenu.cmd
@@ -0,0 +1,53 @@
+{
+    "Name": "DailyCopierSubmenu",
+    "InferenceName": "CopierMenu",
+    "Sequence": [
+        {
+        "Name": "S1: Select and start a task",
+        "KeyBody": {
+                "Conditions": { "Allow": {"Tags": "TaskName"} },
+                "Actions": [
+                    {
+                        "Name":"A1: Find a task",
+                        "Command": "Find",
+                        "FocusArea": [0.4, 0, 0.6, 1],
+                        "PostloadTime": 1000,
+                        "SubActions": {
+                                "Name":"Quit task selected mode before judge",
+                                "Command": "Click",
+                                "PostloadTime": 1000,
+                                "StartOffset": [ 0, -0.5 ]
+                            },
+                        "JudgeConditions":{
+                            "Allow": {"Tags": "TimesOn"},
+                            "FocusArea": [0.4, 0, 0.6, 1]
+                        }
+                    },
+                    {
+                        "Name":"A2: Select the task",
+                        "Command": "Click",
+                        "PostloadTime": 1000,
+                        "FocusArea": [0.4, 0, 0.6, 1]
+                    },
+                    {
+                        "Name":"A3: Start the task",
+                        "Command": "Click",
+                        "StartTag": "CopierGo",
+                        "PostloadTime": 1000
+                    }
+                ]
+            }
+        },
+        {
+            "Name": "S2: Gernal confirmation",
+            "KeyBody": {
+                "Conditions": { 
+                    "Allow": {"Tags": ["iConfirm", "iCancel", "iReject"]} 
+                },
+                "Actions": { "Command": "Click" }
+            }
+        }
+    ]
+}
+
+
